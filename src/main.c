5c3393a21a9a1b8d13dcdedfe4c5b50030a6cff5
Joshua Songy <joshua.songy@gmail.com>
2012-07-02 16:32:39 -0700

    Spelling correction.

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 
 int main(int argc, char** argv) {
 	if(argc < 2) {
-		printf("Useage: %s <filename>\n",argv[0]);
+		printf("Usage: %s <filename>\n",argv[0]);
 		exit(1);
 	}
 	// Read the story file into ram.
