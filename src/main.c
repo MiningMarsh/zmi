9285b55a0efd058be3dcd5973d876ba3de25471f
Bryan Hoyle <starfoxprime@gmail.com>
2012-07-02 19:18:27 -0400

    gitignore and better useage things

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 
 int main(int argc, char** argv) {
 	if(argc < 2) {
-		printf("No story file specified.\n");
+		printf("Useage: %s <filename>\n",argv[0]);
 		exit(1);
 	}
 	// Read the story file into ram.
