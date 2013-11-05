eaf727e209f653d258630ba52f4ebc4fdb2c9a8b
Joshua Songy <joshua.songy@gmail.com>
2013-11-04 21:02:57 -0500

    Small platform fix.

--- include/platform.h
+++ include/platform.h
@@ -60,30 +60,30 @@
  * part of the language specification that these should be defined.
  */
 #if defined(__STDC__) || defined(__STDC_VERSION__)
-#	define PLATFORM_C = 1
+#	define PLATFORM_C 1
 #	if defined(__STDC__)
 #		undef PLATFORM_C89
 #		undef PLATFORM_C
-#		define PLATFORM_C89 = 1
-#		define PLATFORM_C = 89
+#		define PLATFORM_C89 1
+#		define PLATFORM_C 89
 #	endif
 #	if defined(__STDC_VERSION__) && (7- __STDC_VERSION__ -7 == 14)
 #		undef PLATFORM_C89
 #		undef PLATFORM_C90
 #		undef PLATFORM_C
-#		define PLATFORM_C89 = 1
-#		define PLATFORM_C90 = 1
-#		define PLATFORM_C = 90
+#		define PLATFORM_C89 1
+#		define PLATFORM_C90 1
+#		define PLATFORM_C 90
 #	endif
 #   if defined(__STDC_VERSION__) && (__STDC_VERSION__ == 199409L)
 #		undef PLATFORM_C89
 #		undef PLATFORM_C90
 #		undef PLATFORM_C94
 #		undef PLATFORM_C
-#		define PLATFORM_C89 = 1
-#		define PLATFORM_C90 = 1
-#		define PLATFORM_C94 = 1
-#		define PLATFORM_C = 94
+#		define PLATFORM_C89 1
+#		define PLATFORM_C90 1
+#		define PLATFORM_C94 1
+#		define PLATFORM_C 94
 #	endif
 #   if defined(__STDC_VERSION__) && (__STDC_VERSION__ == 199901L)
 #		undef PLATFORM_C89
@@ -91,11 +91,11 @@
 #		undef PLATFORM_C94
 #		undef PLATFORM_C99
 #		undef PLATFORM_C
-#		define PLATFORM_C89 = 1
-#		define PLATFORM_C90 = 1
-#		define PLATFORM_C94 = 1
-#		define PLATFORM_C99 = 1
-#		define PLATFORM_C = 99
+#		define PLATFORM_C89 1
+#		define PLATFORM_C90 1
+#		define PLATFORM_C94 1
+#		define PLATFORM_C99 1
+#		define PLATFORM_C 99
 #	endif
 #   if defined(__STDC_VERSION__) && (__STDC_VERSION__ == 201112L)
 #		undef PLATFORM_C89
@@ -104,12 +104,12 @@
 #		undef PLATFORM_C99
 #		undef PLATFORM_C11
 #		undef PLATFORM_C
-#		define PLATFORM_C89 = 1
-#		define PLATFORM_C90 = 1
-#		define PLATFORM_C94 = 1
-#		define PLATFORM_C99 = 1
-#		define PLATFORM_C11 = 1
-#		define PLATFORM_C = 11
+#		define PLATFORM_C89 1
+#		define PLATFORM_C90 1
+#		define PLATFORM_C94 1
+#		define PLATFORM_C99 1
+#		define PLATFORM_C11 1
+#		define PLATFORM_C 11
 #	endif
 #endif
 
@@ -119,35 +119,54 @@
 #	undef PLATFORM_C94
 #	undef PLATFORM_C11
 #	undef PLATFORM_C
-#	define PLATFORM_CPP = 1
+#	define PLATFORM_CPP 1
 #	if __cplusplus == 199711L
 #		undef PLATFORM_CPP98
 #		undef PLATFORM_CPP
-#		define PLATFORM_CPP98 = 1
-#		define PLATFORM_CPP = 98
+#		define PLATFORM_CPP98 1
+#		define PLATFORM_CPP 98
 #	endif
 #	if __cplusplus == 199707L
 #		undef PLATFORM_CPP98
 #		undef PLATFORM_CPP
-#		define PLATFORM_CPP98 = 1
-#		define PLATFORM_CPP = 98
+#		define PLATFORM_CPP98 1
+#		define PLATFORM_CPP 98
 #	endif
 #	if __cplusplus == 201103L
 #		undef PLATFORM_CPP98
 #		undef PLATFORM_CPP11
 #		undef PLATFORM_CPP
-#		define PLATFORM_CPP98 = 1
-#		define PLATFORM_CPP11 = 1
-#		define PLATFORM_CPP = 11
+#		define PLATFORM_CPP98 1
+#		define PLATFORM_CPP11 1
+#		define PLATFORM_CPP 11
 #	endif
 #	if defined(__GXX_EXPERIMENTAL_CXX0X__)
 #		undef PLATFORM_CPP98
 #		undef PLATFORM_CPP11
 #		undef PLATFORM_CPP
-#		define PLATFORM_CPP98 = 1
-#		define PLATFORM_CPP11 = 1
-#		define PLATFORM_CPP = 11
+#		define PLATFORM_CPP98 1
+#		define PLATFORM_CPP11 1
+#		define PLATFORM_CPP 11
 #	endif
+#endif
+
+/*
+ * Check whether 64bit or 32bit.
+ */ 
+#if __WORDSIZE == 64
+#   undef PLATFORM_BIT
+#   undef PLATFORM_16BIT
+#   undef PLATFORM_32BIT
+#   undef PLATFORM_64BIT
+#	define PLATFORM_BIT 64
+#	define PLATFORM_64BIT
+#else
+#   undef PLATFORM_BIT
+#   undef PLATFORM_16BIT
+#   undef PLATFORM_32BIT
+#   undef PLATFORM_64BIT
+#	define PLATFORM_BIT 32
+#	define PLATFORM_32BIT
 #endif
 
 /*
@@ -156,8 +175,8 @@
 #if defined(__embedded_cplusplus)
 #	undef PLATFORM_CPP
 #	undef PLATFORM_EMBEDDED
-#	define PLATFORM_EMBEDDED = 1
-#	define PLATFORM_CPP = 1
+#	define PLATFORM_EMBEDDED 1
+#	define PLATFORM_CPP 1
 #endif
 
 /*
@@ -167,9 +186,9 @@
 #	undef PLATFORM_LINUX
 #	undef PLATFORM_TARGET
 #	undef PLATFORM_GNU
-#	define PLATFORM_LINUX = 1
-#	define PLATFORM_TARGET = linux
-#	define PLATFORM_GNU = 1
+#	define PLATFORM_LINUX 1
+#	define PLATFORM_TARGET linux
+#	define PLATFORM_GNU 1
 #endif
 
 /*
@@ -178,22 +197,22 @@
 #if defined(__linux__)
 #	undef PLATFORM_LINUX
 #	undef PLATFORM_TARGET
-#	define PLATFORM_LINUX = 1
-#	define PLATFORM_TARGET = linux
+#	define PLATFORM_LINUX 1
+#	define PLATFORM_TARGET linux
 #endif
 
 #if defined(linux)
 #	undef PLATFORM_LINUX
 #	undef PLATFORM_TARGET
-#	define PLATFORM_LINUX = 1
-#	define PLATFORM_TARGET = linux
+#	define PLATFORM_LINUX 1
+#	define PLATFORM_TARGET linux
 #endif
 
 #if defined(__linux)
 #	undef PLATFORM_LINUX
 #	undef PLATFORM_TARGET
-#	define PLATFORM_LINUX = 1
-#	define PLATFORM_TARGET = linux
+#	define PLATFORM_LINUX 1
+#	define PLATFORM_TARGET linux
 #endif
 
 /*
@@ -206,10 +225,10 @@
 #	undef PLATFORM_16BIT
 #	undef PLATFORM_32BIT
 #	undef PLATFORM_64BIT
-#	define PLATFORM_WINDOWS = 1
-#	define PLATFORM_16BIT = 1
-#	define PLATFORM_TARGET = windows
-#	define PLATFORM_BIT = 16
+#	define PLATFORM_WINDOWS 1
+#	define PLATFORM_16BIT 1
+#	define PLATFORM_TARGET windows
+#	define PLATFORM_BIT 16
 #endif
 
 /*
@@ -222,10 +241,10 @@
 #	undef PLATFORM_16BIT
 #	undef PLATFORM_32BIT
 #	undef PLATFORM_64BIT
-#	define PLATFORM_WINDOWS = 1
-#	define PLATFORM_32BIT = 1
-#	define PLATFORM_TARGET = windows
-#	define PLATFORM_BIT = 32
+#	define PLATFORM_WINDOWS 1
+#	define PLATFORM_32BIT 1
+#	define PLATFORM_TARGET windows
+#	define PLATFORM_BIT 32
 #endif
 
 /*
@@ -238,10 +257,10 @@
 #	undef PLATFORM_16BIT
 #	undef PLATFORM_32BIT
 #	undef PLATFORM_64BIT
-#	define PLATFORM_WINDOWS = 1
-#	define PLATFORM_32BIT = 1
-#	define PLATFORM_TARGET = windows
-#	define PLATFORM_BIT = 32
+#	define PLATFORM_WINDOWS 1
+#	define PLATFORM_32BIT 1
+#	define PLATFORM_TARGET windows
+#	define PLATFORM_BIT 32
 #endif
 
 /*
@@ -254,10 +273,10 @@
 #	undef PLATFORM_16BIT
 #	undef PLATFORM_32BIT
 #	undef PLATFORM_64BIT
-#	define PLATFORM_WINDOWS = 1
-#	define PLATFORM_BIT = 64
-#	define PLATFORM_64BIT = 1
-#	define PLATFORM_TARGET = windows
+#	define PLATFORM_WINDOWS 1
+#	define PLATFORM_BIT 64
+#	define PLATFORM_64BIT 1
+#	define PLATFORM_TARGET windows
 #endif
 
 /*
@@ -266,8 +285,8 @@
 #if defined(__WINDOWS__)
 #	undef PLATFORM_WINDOWS
 #	undef PLATFORM_TARGET
-#	define PLATFORM_WINDOWS = 1
-#	define PLATFORM_TARGET = windows
+#	define PLATFORM_WINDOWS 1
+#	define PLATFORM_TARGET windows
 #endif
 
 /*
@@ -276,8 +295,8 @@
 #if defined(__TOS_WIN__)
 #	undef PLATFORM_WINDOWS
 #	undef PLATFORM_TARGET
-#	define PLATFORM_WINDOWS = 1
-#	define PLATFORM_TARGET = windows
+#	define PLATFORM_WINDOWS 1
+#	define PLATFORM_TARGET windows
 #endif
 
 #endif /* PLATFORM_H_ */
